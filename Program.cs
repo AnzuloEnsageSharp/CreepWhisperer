b0af857baa6a457219c0b8939437f64793de4387
Anzulo <anzulchen@gmail.com>
2015-12-29 07:39:33 +0100

    keys are now holdable

--- Program.cs
+++ Program.cs
@@ -22,16 +22,26 @@ namespace CreepWhisperer
         private static readonly MenuItem AggroRangeEffectItem =
             new MenuItem("aggroRangeEffect", "Mark aggroable creeps").SetValue(true);
 
+        private static readonly MenuItem AttackCooldown =
+            new MenuItem("AttackCooldown", "AttackCooldown").SetValue(new Slider(1000, 0, 2000));
+
         private static readonly Dictionary<Unit, ParticleEffect> Effects = new Dictionary<Unit, ParticleEffect>();
 
+        private static Vector3 _currentMovePosition = new Vector3(0, 0, 0);
+
         private static bool _isAttacking = false;
+        private static Vector3 _facingDirection;
+
         public static void Main(string[] args)
         {
+            AggroRangeEffectItem.ValueChanged += Item_ValueChanged;
+
             Menu.AddItem(AggroRangeEffectItem);
             Menu.AddItem(AggroKeyItem);
             Menu.AddItem(UnaggroKeyItem);
+            Menu.AddItem(AttackCooldown);
             Menu.AddToMainMenu();
-
+            
             Game.OnIngameUpdate += Game_OnUpdate;
         }
 
@@ -63,52 +73,60 @@ namespace CreepWhisperer
             if (_isAttacking)
             {
                 _isAttacking = false;
-                me.Hold();
-                // TODO: Continue previous action
+                me.Move(_facingDirection);
+                me.Hold(true);
             }
 
-            var creeps = ObjectMgr.GetEntities<Creep>();
+            var creeps = ObjectMgr.GetEntities<Creep>().ToList();
 
-            if (Utils.SleepCheck("aggroSleep"))
+            if (Utils.SleepCheck("attackSleep"))
             {
+                Unit target = null;
+
                 // aggro
                 if (Game.IsKeyDown(AggroKeyItem.GetValue<KeyBind>().Key))
                 {
-                    var enemy = GetHeroes(me).FirstOrDefault(x => x.Team != me.Team);
-                    me.Attack(enemy);
-                    _isAttacking = true;
+                    target = GetHeroes(me).FirstOrDefault(x => x.Team != me.Team);
                 }
 
                 // unaggro
                 if (Game.IsKeyDown(UnaggroKeyItem.GetValue<KeyBind>().Key))
                 {
-                    var ally = GetHeroes(me).FirstOrDefault(x => x.Team == me.Team);
-                    me.Attack(ally);
-                    _isAttacking = true;
+                    target = GetHeroes(me).FirstOrDefault(x => x.Team == me.Team);
                 }
 
+                if (target != null)
+                {
+                    me.Attack(target);
+                    _facingDirection = Prediction.InFront(me, 10);
+                    _isAttacking = true;
+                    Utils.Sleep(Game.Ping + AttackCooldown.GetValue<Slider>().Value, "attackSleep");
+                }
+            }
+
+            if (Utils.SleepCheck("aggroDrawing"))
+            {
                 // apply range effect
                 foreach (var creep in creeps)
                 {
                     HandleEffect(creep, me);
                 }
 
-                Utils.Sleep(Game.Ping, "aggroSleep");
+                Utils.Sleep(Game.Ping + 200, "aggroDrawing");
             }
-
         }
 
         private static bool IsAggroable(Unit x, Hero me)
         {
-            return x != null && x.IsValid && x.IsAlive && x.Team != me.Team 
+            return x != null && x.IsValid && x.IsAlive && x.Team != me.Team
                 && me.Distance2D(x) <= 500 && me.IsVisibleToEnemies;
         }
 
-        private static IOrderedEnumerable<Hero> GetHeroes(Hero me)
+        private static List<Hero> GetHeroes(Hero me)
         {
             return ObjectMgr.GetEntities<Hero>()
                 .Where(x => x != null && x.IsValid && x.IsAlive && x.IsVisible)
-                .OrderBy(me.Distance2D);
+                .OrderBy(me.Distance2D).ToList();
         }
 
         static void HandleEffect(Unit unit, Hero me)
@@ -132,6 +150,6 @@ namespace CreepWhisperer
                 }
             }
         }
-        
+
     }
 }
